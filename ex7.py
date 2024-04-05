6a5b14aa6e7bda8ba09ee70bd40ad28f27cbfcb4
Станислав <stasuha200@mail.ru>
2024-04-05 22:45:09 +0700

    exercise 7.2

--- ex7.py
+++ ex7.py
@@ -1,10 +1,13 @@
 n = int(input())
 flag = True
+k = 0
 while n != 1:
     if int(n / 2) != n / 2:
-        print('Неверно')
         flag = False
         break
     n = n / 2
-if flag:
+    k += 1
+if flag and k > 0:
     print('Верно')
+else:
+    print('Неверно')
